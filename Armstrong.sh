ddacbf3ce938fa4fa82a633cc179e0cd2a5289a9
SebinGeorge13 <136972860+SebinGeorge13@users.noreply.github.com>
2024-08-15 13:17:33 +0530

    Armstrong.sh

---
+++ Armstrong.sh
@@ -0,0 +1,21 @@
+echo "Enter the Number"
+read n
+y=$n
+s=0
+k=10 
+
+while [ $n -gt 0 ]
+do
+r=$((n % k))
+i=$((r * r * r))
+s=$((s + i))
+n=$((n / k ))
+done
+
+echo $s
+if [ $s == $y ]
+then  
+echo "$s is an Armstrong number"
+else
+echo "$s is not an Armstrong number"
+fi
